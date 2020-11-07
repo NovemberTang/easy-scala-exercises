e4271adbf128a37403d48e776a0ec40c1d927dae
NovemberTang <n.thrale@protonmail.com>
2020-11-07 20:56:27 +0000

    Super reduce challenge

--- src/main/scala/codechallenge/SuperReduceStrings.scala
+++ src/main/scala/codechallenge/SuperReduceStrings.scala
@@ -2,4 +2,14 @@ package codechallenge
 
 object SuperReduceStrings {
 
+  def superReduce(string: String): String = {
+
+    def stringReduce(acc:String, c: Char): String = {
+      if(acc.length > 0 && acc.last == c) acc else acc + c
+    }
+
+    string.foldLeft("")(stringReduce)
+
+  }
+
 }
--- src/test/scala/codechallenge/SuperReduceStringsSpec.scala
+++ src/test/scala/codechallenge/SuperReduceStringsSpec.scala
@@ -1,5 +1,13 @@
 package codechallenge
 
-class SuperReduceStringsSpec {
+import SuperReduceStrings.superReduce
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class SuperReduceStringsSpec extends AnyFlatSpec with Matchers {
+
+  "strings with consecutive duplicates" should "have those duplicates removed" in {
+    superReduce("aaaabccaa") shouldBe "abca"
+  }
 
 }
