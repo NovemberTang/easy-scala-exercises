e3e0429f515c9bf3cb4ffb5841345f7a8eff6616
NovemberTang <n.thrale@protonmail.com>
2020-11-07 18:27:04 +0000

    tidying up

--- src/main/scala/codechallenge/VowelSquare.scala
+++ src/main/scala/codechallenge/VowelSquare.scala
@@ -6,8 +6,8 @@ object VowelSquare {
 
   def findTheVowelSquare(characterMatrix: List[String]): (Int, Int) = {
 
-    val boolMatrix: List[List[Boolean]] = characterMatrix.map(stringToBoolList)
-    val truePairIndices: List[Int] = boolMatrix.map(checkForConsecutiveMatches(_, true))
+    val booleanMatrix: List[List[Boolean]] = characterMatrix.map(stringToBoolList)
+    val truePairIndices: List[Int] = booleanMatrix.map(checkForConsecutiveMatches(_, true))
     val xIndices = truePairIndices.distinct.filterNot(_ == -1)
 
     val squareCoordinates: List[(Int, Int)] = xIndices.map{
@@ -15,26 +15,25 @@ object VowelSquare {
         (x + 1, y + 1)
     }
 
-    squareCoordinates.minBy(tup => pythDistance(tup._1, tup._2))
+    squareCoordinates.minBy(tup => calculatePythagoreanDistance(tup._1, tup._2))
   }
 
-  def charToBool(char: Char): Boolean = {
+  def charIsAVowel(char: Char): Boolean = {
     char match {
       case 'a' | 'e' | 'i' | 'o' | 'u' => true
       case _ => false
     }
   }
 
-  def stringToBoolList(str: String): List[Boolean] = str.map(charToBool).toList
+  def stringToBoolList(str: String): List[Boolean] = str.map(charIsAVowel).toList
 
-  def checkForConsecutiveMatches(list: List[Any], valueToBeChecked: Any): Int = {
+  def checkForConsecutiveMatches(list: List[AnyVal], valueToBeChecked: AnyVal): Int = {
     val pairsToBeChecked = list.sliding(2).toList
     val truePairIndex = pairsToBeChecked.indexOf(List(valueToBeChecked, valueToBeChecked))
     truePairIndex
   }
 
   def square(int: Int): Int = int*int
-  def pythDistance(a:Int, b:Int): Double = sqrt(square(a) + square(b))
-
+  def calculatePythagoreanDistance(a:Int, b:Int): Double = sqrt(square(a) + square(b))
 
 }
