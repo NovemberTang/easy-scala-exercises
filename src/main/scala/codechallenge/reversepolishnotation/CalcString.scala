3d145a199441c7e556d3dc3b713d75e52ee49579
NovemberTang <nthrale@protonmail.com>
2020-11-11 18:46:31 +0000

    reverse polish notation part 2

--- src/main/scala/codechallenge/reversepolishnotation/CalcString.scala
+++ src/main/scala/codechallenge/reversepolishnotation/CalcString.scala
@@ -1,11 +1,32 @@
 package codechallenge.reversepolishnotation
 
-class CalcString(val string: String){
+import scala.annotation.tailrec
+
+class CalcString(val string: String) {
 
   def calc: Double = {
-    val calcList = string.split(" ")
-    val operands = calcList.dropRight(1).map(_.toDouble)
-    val operator = calcList.last
+
+    @tailrec
+    def calcRecurse(string: String): String = {
+      string.length match {
+        case 1 => string
+        case _ => {
+          val (result, charsToDrop) = buildString(string)
+          val newString = result + string.drop(charsToDrop)
+          calcRecurse(newString)
+        }
+      }
+    }
+
+    calcRecurse(string).toDouble
+
+  }
+
+
+  private def calculate(string: String): Double = {
+    val calculationCharList = string.split(" ")
+    val operands = calculationCharList.dropRight(1).map(_.toDouble)
+    val operator = calculationCharList.last
 
     operator match {
       case "+" => operands.sum
@@ -14,6 +35,20 @@ class CalcString(val string: String){
       case "*" => operands.product
     }
 
+  }
+
+  @tailrec
+  private def buildString(inputString: String, outputString: String = ""): (Double, Int) = {
+
+    inputString.head match {
+      case '+' | '-' | '*' | '/' => {
+        val operation = (outputString + inputString.head)
+        val result = calculate(operation)
+        val opLength = operation.length
+        (result, opLength)
+      }
+      case char => buildString(inputString.tail, s"$outputString${char}")
+    }
 
   }
 
--- src/test/scala/codechallenge/ReversePolishNotationSpec.scala
+++ src/test/scala/codechallenge/ReversePolishNotationSpec.scala
@@ -2,20 +2,10 @@ package codechallenge
 
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
-
 import codechallenge.reversepolishnotation.ReversePolishNotation._
 
 class ReversePolishNotationSpec extends AnyFlatSpec with Matchers {
-
-//  "calculateFunction" should "return stack with 2 elements popped and one element pushed" in {
-//    calcSign(List[Double](1, 2, 3, 4, 5), _ / _) should have size 4
-//  }
-//
-//  it should "be use function parameter to calculate value that is pushed" in {
-//    calcSign(List[Double](1, 2, 3, 15, 3), _ / _).last should equal(5)
-//  }
-
-  "calculate" should "be able to calculate single digit numbers" in {
+  "calc" should "be able to calculate single digit numbers" in {
     "1 2 +".calc should equal(3)
   }
 
@@ -31,8 +21,8 @@ class ReversePolishNotationSpec extends AnyFlatSpec with Matchers {
     "-12.9 3 /".calc should equal(-4.3)
   }
 
-//  it should "be able to calculate more complex notations" in {
-//    "1 2 + 4 * 5 + 3 -".calc should equal(14)
-//  }
+  it should "be able to calculate more complex notations" in {
+    "1 2 + 4 * 5 + 3 -".calc should equal(14)
+  }
 
 }
